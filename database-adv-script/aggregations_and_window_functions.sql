482c788c4d9c30e25f753a7a1ccddd870f395373
Shirleen Gatu <wambuimunk@gmail.com>
2025-09-03 03:42:13 +0300

    Add ROW_NUMBER query for property ranking

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -8,7 +8,8 @@ JOIN bookings b
     ON u.id = b.user_id
 GROUP BY u.id, u.name
 ORDER BY total_bookings DESC;
--- Rank properties based on bookings
+
+-- Rank properties based on bookings using RANK()
 SELECT 
     p.id AS property_id,
     p.name AS property_name,
@@ -19,3 +20,16 @@ LEFT JOIN bookings b
     ON p.id = b.property_id
 GROUP BY p.id, p.name
 ORDER BY property_rank;
+
+-- Rank properties based on bookings using ROW_NUMBER()
+SELECT 
+    p.id AS property_id,
+    p.name AS property_name,
+    COUNT(b.id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS property_row_number
+FROM properties p
+LEFT JOIN bookings b 
+    ON p.id = b.property_id
+GROUP BY p.id, p.name
+ORDER BY property_row_number;
+
